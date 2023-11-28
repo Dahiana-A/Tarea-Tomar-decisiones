1fcefa15ab2fc3ce48da646e298c1b51376679c4
Dahiana-A <151797456+Dahiana-A@users.noreply.github.com>
2023-11-27 22:02:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+
+if (n1 >= n2 && n1 >= n3) {
+    console.log (n1);
+} else if (n2 >= n1 && n2 >= n3) {
+    console.log (n2);
+} else {
+    console.log (n3);
+}
